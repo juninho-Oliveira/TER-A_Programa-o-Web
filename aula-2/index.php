79212855afa9c03c9f18c770b4673de5ba49fe51
DAMIAO JUNIOR OLIVEIRA CALADO <damiao.calado@cs.unicid.edu.br>
2025-08-19 20:40:39 -0300

    add aula2

---
+++ aula-2/index.php
@@ -0,0 +1,28 @@
+<?php
+$name = "Junior";
+$idade = "27";
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula-2</title>
+</head>
+
+<body>
+    <?php
+    //Este bloco está no BODY
+    echo "Hellor World !!!";
+    echo "<br/>";
+    echo "Nome: " . $name . " Idade: " . $idade;
+    ?>
+</body>
+
+</html>
