eef2682324386f5e2f9a20effccf96c0ad346207
junior <junior@junior.com>
2025-11-11 21:41:56 -0300

    add

--- usuario_consulta.php
+++ usuario_consulta.php
@@ -76,3 +76,66 @@ echo "<td>". $reg_consulta["email"] . "</td>";
 			</table>
 	</body>
 </html>
+
+create database aula_php_junior;
+use aula_php_junior; -- use é o comando para 
+-- ultilizar um banco de dados 
+
+create table usuarios (
+	id_usuario int not null auto_increment primary key,
+    nome_usuario varchar(200),
+    usuario varchar(10) unique,
+    senha varchar(20),
+    email varchar(200),
+    cpf char(11),
+    dt_nascimento date,
+    tipo_usuario varchar(10),
+    status char(1)
+);
+
+describe usuarios;
+
+insert into usuarios values(
+	null,
+    'Junior',
+    'juninho123',
+    '123456', -- senha em ambiente real é HASH
+    'junior@unicid.edu.br',
+    '1234567891011',
+    '2020-12-18', -- ano mês dia
+    'admin',
+    'a'
+);
+
+use aula_php_junior;
+show databases;
+show tables;
+
+describe usuarios;
+select * from usuarios;
+
+
+select nome_usuario, senha from usuarios usuarios 
+where
+email = 'junior@unicid.edu.br'
+and
+(usuario = 'juninho123' and senha = 123456);
+
+update usuarios set nome_usuario ='junior' where id_usuario = 3 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
